ecef9812b5c26710be994c27ec435d7ef68e814a
ageorgief <andreyg673@gmail.com>
2021-05-02 19:16:11 +0300

    add implementation for commandfactory

--- SplitWiseServer/src/bg/sofia/uni/fmi/mjt/server/commands/CommandFactory.java
+++ SplitWiseServer/src/bg/sofia/uni/fmi/mjt/server/commands/CommandFactory.java
@@ -1,5 +1,36 @@
 package bg.sofia.uni.fmi.mjt.server.commands;
 
 
+import bg.sofia.uni.fmi.mjt.server.cache.Cache;
+import bg.sofia.uni.fmi.mjt.server.exception.InvalidCommandException;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CommandFactory {
+    private Map<String, Command> commands;
+
+    public CommandFactory(Cache serverCache) {
+        commands = new HashMap<>();
+        commands.put("register", new Register(serverCache));
+        commands.put("login", new Login(serverCache));
+        commands.put("add-friend", new AddFriend(serverCache));
+        commands.put("create-group", new CreateGroup(serverCache));
+        commands.put("split", new Split(serverCache));
+        commands.put("split-group", new SplitGroup(serverCache));
+        commands.put("get-status", new GetStatus(serverCache));
+        commands.put("payed", new Payed(serverCache));
+    }
+
+    public Command make(String commandName) throws InvalidCommandException {
+        Command command = commands.get(commandName);
+
+        if (command == null) {
+            String message = "The entered command is invalid.";
+
+            throw new InvalidCommandException(message);
+        }
+
+        return command;
+    }
 }
