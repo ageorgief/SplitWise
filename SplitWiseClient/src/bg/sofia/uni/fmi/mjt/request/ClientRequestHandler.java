42265b33b4782b5ef5bc1ef32b94e8d82b463aac
ageorgief <andreyg673@gmail.com>
2021-05-03 09:10:57 +0300

    add implementation for clientrequesthandler

---
+++ SplitWiseClient/src/bg/sofia/uni/fmi/mjt/request/ClientRequestHandler.java
@@ -0,0 +1,29 @@
+package bg.sofia.uni.fmi.mjt.request;
+
+import java.io.*;
+
+public class ClientRequestHandler implements Runnable {
+    private InputStream input;
+    private Writer output;
+
+    public ClientRequestHandler(InputStream input, Writer output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    @Override
+    public void run() {
+        try (var reader = new BufferedReader(new InputStreamReader(input));
+             var writer = new PrintWriter(output, true)) {
+            //TODO: add break point for loop
+            while (true) {
+                String clientRequest = reader.readLine();
+                writer.println(clientRequest);
+            }
+        } catch (IOException e) {
+            System.out.println("Problem occurred while sending request!");
+            System.out.println("Please refresh your connection");
+        }
+    }
+
+}
